aa43e6ba2dd286d2caabbbc6bc0227765dba3781
callumacrae <callum@lynxphp.com>
2014-02-10 16:15:19 +0000

    Made enemies static

--- Enemy.java
+++ Enemy.java
@@ -1,12 +1,12 @@
-import city.cs.engine.DynamicBody;
 import city.cs.engine.PolygonShape;
 import city.cs.engine.Shape;
+import city.cs.engine.StaticBody;
 import city.cs.engine.World;
 
 /**
  * Create a new DynamicBody for an enemy.
  */
-public class Enemy extends DynamicBody {
+public class Enemy extends StaticBody {
 	static final Shape shape = new PolygonShape(0.149f, 0.975f, 0.775f, 0.193f, 0.772f, -0.099f, 0.401f, -0.928f, -0.36f, -0.922f, -0.719f, -0.025f, -0.725f, 0.163f, -0.14f, 0.972f);
 
 	/**
@@ -18,7 +18,5 @@ public class Enemy extends DynamicBody {
 		super(world, shape);
 
 //		this.setImage(new BodyImage("data/yellow-bird.gif", 2.25f));
-		this.setGravityScale(10);
-		this.setFixedRotation(true);
 	}
 }
